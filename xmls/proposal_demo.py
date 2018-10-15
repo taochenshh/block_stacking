01062fe6bf910d2fa19ef6b72790d122b58bf207
Tao Chen <chentao904@163.com>
2018-10-15 16:36:21 -0400

    update xml

---
+++ xmls/proposal_demo.py
@@ -0,0 +1,39 @@
+from mujoco_py import load_model_from_path, MjSim, MjViewer
+import numpy as np
+import time
+
+env_file = 'block_world.xml'
+model = load_model_from_path(env_file)
+sim = MjSim(model, nsubsteps=1)
+sim.model.vis.map.znear = 0.02
+sim.model.vis.map.zfar = 50.0
+viewer = MjViewer(sim)
+viewer.cam.lookat[:3] = np.array([0, 0, 0.1])
+viewer.cam.distance = 2
+viewer.cam.elevation = -20
+# sim.model.body_pos[1] = np.array([0.6, 0.6, 4])
+sim.reset()
+cube_pose = sim.data.get_joint_qpos('cube_0')
+cube_pose[:3] = np.array([0, 0, 0.02])
+sim.data.set_joint_qpos('cube_0', cube_pose)
+
+cube_pose = sim.data.get_joint_qpos('cuboid_0')
+cube_pose[:3] = np.array([0, 0, 0.06])
+sim.data.set_joint_qpos('cuboid_0', cube_pose)
+
+cube_pose = sim.data.get_joint_qpos('cube_1')
+cube_pose[:3] = np.array([0.12, 0, 0.02])
+sim.data.set_joint_qpos('cube_1', cube_pose)
+
+cube_pose = sim.data.get_joint_qpos('cube_2')
+cube_pose[:3] = np.array([0.08, 0, 0.10])
+sim.data.set_joint_qpos('cube_2', cube_pose)
+
+cube_pose = sim.data.get_joint_qpos('cube_3')
+cube_pose[:3] = np.array([-0.08, 0, 0.10])
+sim.data.set_joint_qpos('cube_3', cube_pose)
+sim.forward()
+
+while True:
+    sim.step()
+    viewer.render()
--- xmls/show_env.py
+++ xmls/show_env.py
@@ -9,10 +9,17 @@ sim.model.vis.map.zfar = 50.0
 viewer = MjViewer(sim)
 
 # sim.model.body_pos[1] = np.array([0.6, 0.6, 4])
-print(sim.data.get_joint_qpos('cube_0'))
+sim.reset()
+cube_pose = sim.data.get_joint_qpos('cube_0')
+print(cube_pose)
+# cube_pose[:3] += 0.1
+sim.data.set_joint_qpos('cube_0', cube_pose)
+sim.forward()
+cube_pose = sim.data.get_joint_qpos('cube_0')
+print(cube_pose)
 print(sim.model.body_pos[1])
 print(sim.model.geom_pos[0])
-sim.reset()
+
 while True:
     # print(sim.model.body_pos[1], sim.data.body_xpos[1])
     sim.step()
